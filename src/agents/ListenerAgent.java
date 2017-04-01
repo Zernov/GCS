0c32cc73b5579166a112c8824684eb0dbc5b878c
Zernov <gzernov@gmail.com>
2017-04-01 19:59:22 +0300

    7:59

--- src/agents/ListenerAgent.java
+++ src/agents/ListenerAgent.java
@@ -1,10 +1,13 @@
 package agents;
 
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 import java.util.TreeMap;
 
@@ -21,9 +24,9 @@ public class ListenerAgent extends Agent {
         Object[] args = getArguments();
         if (args != null && args.length > 0) {
             preferences = toMap(args[0].toString());
-            System.out.println(String.format("\n[ListenerAgent \"%s\" was created]\n", getLocalName()));
+            System.out.println(String.format("[ListenerAgent \"%s\" was created]\n", getLocalName()));
         } else {
-            System.out.print(String.format("\n[ListenerAgent \"%s\" was not created (wrong arguments)]\n", getLocalName()));
+            System.out.print(String.format("[ListenerAgent \"%s\" was not created (wrong arguments)]\n", getLocalName()));
             doDelete();
         }
 
@@ -36,10 +39,47 @@ public class ListenerAgent extends Agent {
             sd.setType("Listener");
             dfd.addServices(sd);
             DFService.register(this, dfd);
-            System.out.println(String.format("\n[ListenerAgent \"%s\" was registered ]\n", getLocalName()));
+            System.out.println(String.format("[ListenerAgent \"%s\" was registered ]\n", getLocalName()));
         } catch (FIPAException fe) {
             fe.printStackTrace();
         }
         //======================================================//
+
+        //======================Behaviours======================//
+        //Request profit
+        addBehaviour(new CyclicBehaviour(this) {
+            @Override
+            public void action() {
+                ACLMessage msg = receive(MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
+                if (msg != null) {
+                    String content = msg.getContent();
+                    Integer[][] schedule = toArray(content);
+                    Integer profit = 0;
+                    for (Integer[] timezone: schedule) {
+                        Integer max = 0;
+                        for(int i = 0; i < TIMEZONE_SIZE; i++) {
+                            max = Math.max(max, preferences.get(timezone[i]));
+                        }
+                        profit = profit + max;
+                    }
+                    ACLMessage reply = msg.createReply();
+                    reply.setPerformative(ACLMessage.INFORM);
+                    reply.setContent(profit.toString());
+                    myAgent.send(reply);
+                } else {
+                    block();
+                }
+            }
+        });
+        //======================================================//
+    }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+            System.out.println(String.format("[ListenerAgent \"%s\" was deregistered]\n", getLocalName()));
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/agents/ScheduleAgent.java
+++ src/agents/ScheduleAgent.java
@@ -20,9 +20,9 @@ public class ScheduleAgent extends Agent {
         Object[] args = getArguments();
         if (args != null && args.length > 0) {
             schedule = toArray(args[0].toString());
-            System.out.println(String.format("\n[ScheduleAgent \"%s\" was created]\n", getLocalName()));
+            System.out.println(String.format("[ScheduleAgent \"%s\" was created]\n", getLocalName()));
         } else {
-            System.out.print(String.format("\n[ScheduleAgent \"%s\" was not created (wrong arguments)]\n", getLocalName()));
+            System.out.print(String.format("[ScheduleAgent \"%s\" was not created (wrong arguments)]\n", getLocalName()));
             doDelete();
         }
 
@@ -35,10 +35,19 @@ public class ScheduleAgent extends Agent {
             sd.setType("Schedule");
             dfd.addServices(sd);
             DFService.register(this, dfd);
-            System.out.println(String.format("\n[ScheduleAgent \"%s\" was registered ]\n", getLocalName()));
+            System.out.println(String.format("[ScheduleAgent \"%s\" was registered ]\n", getLocalName()));
         } catch (FIPAException fe) {
             fe.printStackTrace();
         }
         //======================================================//
+    }
+
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+            System.out.println(String.format("[ScheduleAgent \"%s\" was deregistered]\n", getLocalName()));
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
     }
 }
